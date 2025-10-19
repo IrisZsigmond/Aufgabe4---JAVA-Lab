25560729eef9a8fb891a205834aeac194d3aa0a0
IrisMaria <iris.zsigmond@stud.ubbcluj.ro>
2025-10-19 13:52:22 +0300

    Innitial commit, USBDrive class implemented

---
+++ src/USBDrive.java
@@ -0,0 +1,15 @@
+public class USBDrive {
+    private int price;
+
+    public USBDrive(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
