eb11a0cef76a42f8d5125697528d8bd76163ae6f
IrisMaria <iris.zsigmond@stud.ubbcluj.ro>
2025-10-19 13:53:32 +0300

    Tastatur class implemented

---
+++ src/Tastatur.java
@@ -0,0 +1,15 @@
+public class Tastatur {
+    private int price;
+
+    public Tastatur(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
